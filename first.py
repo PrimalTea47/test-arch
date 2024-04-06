9415ab9ba0c3884be5f2888c87144cfb9a135cbc
PrimalTea47 <leandre.fouret@studednt-oec.fr>
2024-04-06 11:27:09 +0200

    test arch

---
+++ first.py
@@ -0,0 +1,14 @@
+import time
+
+tab = input("name, age > ").split(',')
+
+isAdult = True if int(tab[1]) >=18 else False
+
+if isAdult:
+    print(f'Hi {tab[0].capitalize()}, you\'re an adult !')
+
+else:
+    print(f'Hi {tab[0].capitalize()}, you\'re a child !')
+
+time.sleep(3)
+print(tab)
